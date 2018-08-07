7cc4288c1e7b85141391ee65d91e63cd51e9d578
Tara Tran <taratptran@gmail.com>
2018-08-07 13:51:52 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -16,4 +16,5 @@ end
 
 def bob_the_reins 
 puts "Holding your arms out in front of you, straight and at chest level"
-puts 
+puts "Cross your right wrist over your left and hold them together"
+puts ""
