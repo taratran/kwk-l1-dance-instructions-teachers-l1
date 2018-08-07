ee8f68e4ca5ac1fce43a8bbf03cb0473cc36a5b5
Tara Tran <taratptran@gmail.com>
2018-08-07 13:39:33 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,3 +1,14 @@
 def starting_stance
-  
+  puts "Plant legs far apart, bend knees slightly and keep posture loose"
+end 
+
+def base_footwork 
+  puts "Lift right foot"
+  puts "Return right foot to the ground"
+  #finish step 3 
+end 
+
+def skip_step(starting_foot)
+  puts "Lower #{starting_foot} to the ground"
+  puts "Bounce #{start_foot} back up slightly, kicking it a few inches back"
 end 
