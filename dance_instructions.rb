1d9bbfff57fa39ca5622be4bcf90ac606b3eb5bd
Tara Tran <taratptran@gmail.com>
2018-08-07 13:41:34 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -11,5 +11,5 @@ end
 def skip_step(starting_foot)
   puts "Lower #{starting_foot} to the ground"
   puts "Bounce #{start_foot} back up slightly, kicking it a few inches back"
-puts "Briefly raise the opposite foot, then the #{starting_foot}"
+puts "Briefly raise the opposite foot, then the #{starting_foot} twice more!"
 end 
