97435134719aaca90667f3693f9a03234f527bda
Tara Tran <taratptran@gmail.com>
2018-08-07 13:52:38 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -18,3 +18,4 @@ def bob_the_reins
 puts "Holding your arms out in front of you, straight and at chest level"
 puts "Cross your right wrist over your left and hold them together"
 puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
+puts "Do this 7 more times!" 
